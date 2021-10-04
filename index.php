caaedf09677032f47172b2f3b5bf9fd1387d70f7
Ivo Schwizer <ivoschwizer@macbook-pro-ssd.home>
2021-10-04 09:15:43 +0200

    korrex

--- index.php
+++ index.php
@@ -35,12 +35,12 @@
       <article class="projekt right">
         <img src="img/art-of-loft-neubau-amriswil.jpg" alt="art-of-loft-neubau-amriswil" width="100%" height="auto">
         <h2>Neubau<br>Realisation seit 2021</h2>
-        <h1>4 MFH<br>Amriswil</h1>
+        <h1>3 MFH<br>Amriswil TG</h1>
       </article>
       <article class="projekt center">
         <img src="img/art-of-loft-studie-mfh-wattwil.jpg" alt="art-of-loft-studie-mfh-wattwil" width="100%" height="auto">
         <h2>Studie<br>2020</h2>
-        <h1>MFH<br>Wattwil</h1>
+        <h1>MFH<br> SG</h1>
       </article>
       <article class="projekt full">
         <img src="img/art-of-loft-studie-villa-spanien.jpg" alt="art-of-loft-studie-villa-spanien" width="100%" height="auto">
@@ -49,24 +49,30 @@
       </article>
       <article class="projekt right">
         <img src="img/art-of-loft-studie-herrliberg.jpg" alt="art-of-loft-studie-herrliberg" width="100%" height="auto">
-        <h2>Eingeladener Wettbewerb<br>2015</h2>
-        <h1>Wängirain<br>Herrliberg</h1>
+        <h2>Eingeladener Wettbewerb<br>Realisation 2019 abgeschlossen</h2>
+        <h1>Herrliberg ZH</h1>
       </article>
       <article class="projekt center">
         <img src="img/art-of-loft-eingeladener-wettbewerb.jpg" alt="art-of-loft-eingeladener-wettbewerb" width="100%" height="auto">
         <h2>Eingeladener Wettbewerb<br>2014</h2>
+        <h1>Ermatingen TG</h1>
       </article>
 
       <div id="footer">
-          <h2>
-            <a target="_blank" href="https://www.google.ch/maps/place/Art+of+Loft/@47.4296178,9.3775603,17z/data=!3m1!4b1!4m5!3m4!1s0x479b1e4578691d93:0xb96068c2a06244e9!8m2!3d47.4296961!4d9.3798401">
-              Art of Loft AG<br>
-              Schützengartenweg 3<br>
-              9000 St.Gallen<br><br>
-            </a>
-            <a href="tel:0714140202">T 071 414 02 02</a><br>
-            <a href="mailto:t.von.allmen@art-of-loft.ch" target="_top">Mail</a><br>
-          </h2>
+        <h2 class="pw">
+            16.04.2022<br>
+            Relaunch<br>
+            neue Webseite<br><br>
+        </h2>
+        <h2>
+          <a target="_blank" href="https://www.google.ch/maps/place/Art+of+Loft/@47.4296178,9.3775603,17z/data=!3m1!4b1!4m5!3m4!1s0x479b1e4578691d93:0xb96068c2a06244e9!8m2!3d47.4296961!4d9.3798401">
+            Art of Loft AG<br>
+            Schützengartenweg 3<br>
+            9000 St.Gallen<br><br>
+          </a>
+          <a href="tel:0714140202">T 071 414 02 02</a><br>
+          <a href="mailto:t.von.allmen@art-of-loft.ch" target="_top">Mail</a><br>
+        </h2>
       </div>
     </section>
 
